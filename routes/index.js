3438b98c3bd0a95707d712bbc3251e1ca462168f
Lucas Barros <lbs12@aluno.ifal.edu.br>
2024-10-21 22:25:23 -0300

    fix: index.js correção do index.js com as devidas rotas

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,14 @@
 
 const router = require('express').Router();
 
-// Controller imports
 const { categoryController, productController } = require('../controllers');
 
-// Category routes
 router.get('/v1/categories', categoryController.getAllCategories);
 router.post('/v1/categories', categoryController.createCategory);
 router.get('/v1/categories/:id', categoryController.getCategoryById);
 router.put('/v1/categories/:id', categoryController.updateCategory);
 router.delete('/v1/categories/:id', categoryController.deleteCategory);
 
-// Product routes
 router.get('/v1/products', productController.getAllProducts);
 router.post('/v1/products', productController.createProduct);
 router.get('/v1/products/:id', productController.getProductById);
